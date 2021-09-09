a0655b1fc6ec054c31d887d90d01bb43e9d072d8
Daniil-Perevezentsev <81754323+Daniil-Perevezentsev@users.noreply.github.com>
2021-09-09 11:16:50 +0300

    Add files via upload

--- calc Perevezentsev & Konik/calc Perevezentsev & Konik/ViewController.swift
+++ calc Perevezentsev & Konik/calc Perevezentsev & Konik/ViewController.swift
@@ -64,7 +64,37 @@ class ViewController: UIViewController {
             resultLabel.text = String(result)
         }
         }
+    
+    @IBDesignable class RoundButton : UIButton{
+
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            sharedInit()
+        }
+
+        required init?(coder aDecoder: NSCoder) {
+            super.init(coder: aDecoder)
+            sharedInit()
+        }
+
+        override func prepareForInterfaceBuilder() {
+            sharedInit()
+        }
+
+        func sharedInit() {
+            refreshCorners(value: cornerRadius)
+        }
+
+        func refreshCorners(value: CGFloat) {
+            layer.cornerRadius = value
+        }
+
+        @IBInspectable var cornerRadius: CGFloat = 15 {
+            didSet {
+                refreshCorners(value: cornerRadius)
+            }
+        }
 }
 
 
-
+}
